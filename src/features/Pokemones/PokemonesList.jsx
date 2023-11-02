2dd89db48fd70ba5003695726a52f008714a7da3
emanuelcardozo <leuname.01@gmail.com>
2023-11-02 15:51:06 -0300

    temp

---
+++ src/features/Pokemones/PokemonesList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function PokemonesList(props) {
+  return (
+    <div>PokemonesList</div>
+  )
+}
+
+PokemonesList.propTypes = {}
+
+export default PokemonesList
---
+++ src/features/{{cookiecutter.resource_name}}/{{cookiecutter.resource_name}}List.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function {{cookiecutter.resource_name}}List(props) {
+  return (
+    <div>{{cookiecutter.resource_name}}List</div>
+  )
+}
+
+{{cookiecutter.resource_name}}List.propTypes = {}
+
+export default {{cookiecutter.resource_name}}List
